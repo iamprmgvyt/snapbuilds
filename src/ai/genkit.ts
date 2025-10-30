eedd905957856f98bbc4676af75888a16156fe51
Gia Bao Nguyen <thuvien040281@gmail.com>
2025-10-30 15:32:08 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ge

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,6 @@
 import {genkit} from 'genkit';
 import {groq} from 'genkitx-groq';
-import {llama3} from 'genkitx-groq/llms';
 
 export const ai = genkit({
   plugins: [groq({apiKey: process.env.GROQ_API_KEY})],
 });
-
-export {llama3};
